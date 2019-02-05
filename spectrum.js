e6613c7e7172488831808ff84e43aad185bbe739
Solusverd22 <32770672+Solusverd22@users.noreply.github.com>
2019-02-05 17:46:17 +0000

    added colours

--- spectrum.js
+++ spectrum.js
@@ -6,11 +6,12 @@ function Spectrum(){
 		var spectrum = fourier.analyze();
 		noStroke();
 		
-		fill(0,255,0)
 		for (var i = 0; i< spectrum.length; i++){
 			var y = map(i, 0, spectrum.length, 0, height);
-		    var w = map(spectrum[i], 0, 255, 0,width);
-		    rect(0,y,w, height / spectrum.length );
+			var w = map(spectrum[i], 0, 255, 0,width);
+
+			fill(spectrum[i],255-spectrum[i],0);
+			rect(0,y,w, height / spectrum.length );
   		}
 	
 		pop();
