399708ebd0e646585ba3b81e2e0c776a6b0a8776
crouton99 <callumpwdownes@gmail.com>
2019-02-05 17:23:46 +0000

    Initial commit

---
+++ spectrum.js
@@ -0,0 +1,18 @@
+function Spectrum(){
+	this.name = "spectrum";
+
+	this.draw = function(){
+		push();
+		var spectrum = fourier.analyze();
+		noStroke();
+		
+		fill(0,255,0)
+		for (var i = 0; i< spectrum.length; i++){
+			var y = map(i, 0, spectrum.length, 0, height);
+		    var w = map(spectrum[i], 0, 255, 0,width);
+		    rect(0,y,w, height / spectrum.length );
+  		}
+	
+		pop();
+	};
+}
