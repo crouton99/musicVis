33f4d27362e3dd007c74302a7f30afc3bdd8e59f
Solusverd22 <32770672+Solusverd22@users.noreply.github.com>
2019-03-12 17:22:20 +0000

    added template

--- spectrum.js
+++ spectrum.js
@@ -5,15 +5,26 @@ function Spectrum(){
 		push();
 		var spectrum = fourier.analyze();
 		noStroke();
-		
-		for (var i = 0; i< spectrum.length; i++){
-			var y = map(i, 0, spectrum.length, 0, height);
-			var w = map(spectrum[i], 0, 255, 0,width);
+		//fill(0,255,0)
+		// for (var i = 0; i< spectrum.length; i++){
+		// 	var x = map(i, 0, spectrum.length, 0, width);
+		//     var h = -height + map(spectrum[i], 0, 255, height, 0);
+		//     rect(x, height, width / spectrum.length, h );
+  // 		}
 
-			fill(spectrum[i],255-spectrum[i],0);
-			rect(0,y,w, height / spectrum.length );
-  		}
-	
+
+		for(var i = 0; i<spectrum.length; i++){
+
+			//fade the colour of the bin from green to red
+			var g = map(spectrum[i], 0, 255, 255, 0);
+			fill(spectrum[i], g, 0);
+
+			//draw each bin as a rectangle from the left of the screen
+			//across
+			var y = map(i, 0, spectrum.length, 0, height);
+			var w = map(spectrum[i], 0, 255, 0, width);
+			rect(0, y, w, height/spectrum.length);
+		}  		
 		pop();
 	};
 }
